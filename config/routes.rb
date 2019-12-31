e8cabae7ed5f4ec5e27f04fdc2855556c3e8d377
Katie Seggelke <katie.seggelke@gmail.com>
2019-12-31 22:00:21 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   # get 'articles/:id/edit', to: 'articles#edit', as: :edit_article 
   # patch 'articles/:id', to: 'articles#update'
+  
+  resources :articles, only: [:index, :show, :new, :create, :edit, :update]
 
 end
